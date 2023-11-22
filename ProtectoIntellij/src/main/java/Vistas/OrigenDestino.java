29854761aad9e24c27a50ec75882a52d35f8d8a6
Juan Agustin Umaña Silva <juan.agustinsilva1@gmail.com>
2023-11-22 11:24:45 -0300

    Agrega botones

--- ProtectoIntellij/src/main/java/Vistas/OrigenDestino.java
+++ ProtectoIntellij/src/main/java/Vistas/OrigenDestino.java
@@ -7,16 +7,23 @@ import java.awt.event.ActionListener;
 
 public class OrigenDestino extends JPanel {
     private ImageIcon icon = new ImageIcon("src/main/resources/logo1.png");
-    private JButton origen = new JButton();
-    private JButton destino = new JButton();
+    private JButton origen, destino;
     private JButton siguiente = new JButton("Siguiente");
     private JLabel logo;
     public OrigenDestino(){
+        this.setLayout(null);
         this.setBackground(new Color(61, 100, 227));
         this.add(origen);
         this.add(destino);
 
         logo = new JLabel(icon);
         this.add(logo);
+
+        origen = new JButton("Origen");
+        this.add(origen);
+
+        destino = new JButton("Destino");
+        this.add(destino);
+
     }
 }
