cf267480054bfb35eecbfe614b2b2c6ddc656a25
alanyur <alanibagon@gmail.com>
2023-12-12 17:09:41 -0300

    eliminación de variables innecesarias

--- ProtectoIntellij/src/main/java/Codigo/Buses.java
+++ ProtectoIntellij/src/main/java/Codigo/Buses.java
@@ -4,8 +4,6 @@ public abstract class Buses {
     private int asientos;
     private int pisos;
     private String patente;
-    private ciudad origen;
-    private ciudad destino;
 
     public abstract int getAsientos();
 }
